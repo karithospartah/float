696e14b32ef47bf3de73891b83ac9b00e3e5f3b6
karithospartah <85488402+karithospartah@users.noreply.github.com>
2021-07-09 03:11:30 -0700

    Add files via upload

---
+++ ka ri tho.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+ float radius, area;
+printf("Enter the radius of a circle\n");
+scanf("%f", &radius);
+area = 3.142*radius*radius;
+printf("Area of the circle = %.2f\n", area); 
+return 0; 
+}
